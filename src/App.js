57f783f1234b7e5d0066a23513a69e8e99f5eb84
MarianaBirca <82412276+MarianaBirca@users.noreply.github.com>
2022-01-10 21:19:02 +0000

    Added rel attribute to the footer link

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
         <a
           href="https://github.com/MarianaBirca/react-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>

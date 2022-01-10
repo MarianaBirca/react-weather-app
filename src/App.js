1596b8d64159982e90c943b7b421fdcfecb2139f
MarianaBirca <82412276+MarianaBirca@users.noreply.github.com>
2022-01-10 21:12:24 +0000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Mariana Birca and is{" "}
+        <a
+          href="https://github.com/MarianaBirca/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
